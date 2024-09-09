75fd28d9b705dfab99a58d023fe5d39c3edc1ebe
Longfeng Qin <sancaros@msn.cn>
2024-09-10 00:31:40 +0800

    添加项目文件。

---
+++ PSO2SERVER/Database/PolarisEF.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.IO;
+using MySql.Data.EntityFramework;
+using PolarisServer.Models;
+
+namespace PolarisServer.Database
+{
+    public class ServerInfo
+    {
+        [Key, MaxLength(255)]
+        public string Info { get; set; }
+
+        public string Setting { get; set; }
+    }
+
+    public class Teleport
+    {
+        [Key, Column(Order = 1)]
+        public string ZoneName { get; set; }
+
+        [Key, Column(Order = 2)]
+        public int ObjectID { get; set; }
+
+        public float RotX { get; set; }
+        public float RotY { get; set; }
+        public float RotZ { get; set; }
+        public float RotW { get; set; }
+
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
+    }
+
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Nickname { get; set; }
+        public string SettingsIni { get; set; }
+    }
+
+    public class NPC
+    {
+        [Key, Column(Order = 1)]
+        public int EntityID { get; set; }
+        [Key, Column(Order = 2)]
+        public string ZoneName { get; set; }
+
+        public string NPCName { get; set; }
+
+        public float RotX { get; set; }
+        public float RotY { get; set; }
+        public float RotZ { get; set; }
+        public float RotW { get; set; }
+
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
+    }
+
+    public class GameObject
+    {
+        [Key, Column(Order = 1)]
+        public int ObjectID { get; set; }
+        [Key, Column(Order = 2)]
+        public string ZoneName { get; set; }
+
+        public string ObjectName { get; set; }
+
+        public byte[] ObjectFlags { get; set; }
+
+        public float RotX { get; set; }
+        public float RotY { get; set; }
+        public float RotZ { get; set; }
+        public float RotW { get; set; }
+
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
+    }
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class PolarisEf : DbContext
+    {
+        public DbSet<ServerInfo> ServerInfos { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Teleport> Teleports { get; set; }
+        public DbSet<NPC> NPCs { get; set; }
+        public DbSet<GameObject> GameObjects { get; set; }
+
+        public PolarisEf()
+            : base(
+                string.Format("server={0};port={1};database={2};username={3};password={4}",
+                                         PolarisApp.Config.DatabaseAddress,
+                                         PolarisApp.Config.DatabasePort,
+                                         PolarisApp.Config.DatabaseName,
+                                         PolarisApp.Config.DatabaseUsername,
+                                         PolarisApp.Config.DatabasePassword)
+                  )
+        {
+        }
+
+        public void SetupDB()
+        {
+            try
+            {
+                foreach (
+                    var f in
+                        Directory.EnumerateFiles(Directory.GetCurrentDirectory() + "/Resources/sql/scripts/", "*.sql"))
+                {
+                    Logger.WriteInternal("[DB ] 执行数据库脚本 {0}", f);
+                    Database.ExecuteSqlCommand(File.ReadAllText(f));
+                }
+                var revision = ServerInfos.Find("Revision");
+                if (revision == null)
+                {
+                    revision = new ServerInfo { Info = "Revision", Setting = "0" };
+                    ServerInfos.Add(revision);
+
+                    //TODO Possibly move this somewhere else?
+                    Database.ExecuteSqlCommand("ALTER TABLE Players AUTO_INCREMENT=10000000");
+                }
+                SaveChanges();
+
+                Logger.WriteInternal("[DB ] 加载数据集修订的数据库 {0}", revision.Setting);
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteException("数据库异常", ex);
+            }
+        }
+
+        public bool TestDatabaseConnection()
+        {
+            try
+            {
+                using (var context = new PolarisEf())
+                {
+                    // 执行一个简单的查询来测试数据库连接
+                    context.Database.ExecuteSqlCommand("SELECT 1");
+
+                    Logger.WriteInternal("[DB Test] 数据库连接成功。");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteException("[DB Test] 数据库连接异常", ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ ServerTest/Test.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+using PolarisServer.Models;
+using PolarisServer.Packets;
+using PolarisServer.Packets.Handlers;
+
+namespace PolarisTests
+{
+    [TestFixture]
+    public class ReflectionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            PacketHandlers.LoadPacketHandlers();
+        }
+
+        [Test]
+        public void TestLoginLookup()
+        {
+            Assert.IsNotNull(PacketHandlers.GetHandlerFor(0x11, 0x0));
+        }
+
+        [Test]
+        public void TestAllHandlers()
+        {
+            foreach (var p in PacketHandlers.GetLoadedHandlers())
+            {
+                Assert.IsNotNull(p);
+                Assert.IsInstanceOf(typeof(PacketHandler), p, "Loaded PacketHandler is not a Packet Handler!");
+            }
+        }
+    }
+
+    [TestFixture]
+    public class UnsafeTests
+    {
+        private readonly Character.JobParam _jp = new Character.JobParam();
+
+        [Test]
+        public void CheckJobParam()
+        {
+            var size = Marshal.SizeOf(typeof(Character.JobParam));
+            Assert.IsNotNull(_jp);
+            var jpArr = new byte[size];
+            var ptr = Marshal.AllocHGlobal(size);
+
+            Marshal.StructureToPtr(_jp, ptr, true);
+            Marshal.Copy(ptr, jpArr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+
+            foreach (var b in jpArr)
+            {
+                Assert.AreEqual(0, b);
+            }
+            Assert.AreEqual(size, jpArr.Length);
+        }
+    }
+
+    [TestFixture]
+    public class WriterTests
+    {
+        private PacketWriter _writer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _writer = new PacketWriter();
+        }
+
+        [Test]
+        public void TestStructureWrite()
+        {
+            var structureSize = Marshal.SizeOf(typeof(Character.JobParam));
+            var jp = new Character.JobParam();
+            jp.entries.hunter.level = 7;
+            _writer.WriteStruct(jp);
+            var structArray = _writer.ToArray();
+            Assert.AreEqual(structureSize, structArray.Length);
+            Assert.AreEqual(7, structArray[12]);
+        }
+    }
+
+    [TestFixture]
+    public class JsonTests
+    {
+        [Test]
+        public void TestObjectSerialize()
+        {
+            var testObject = new PSOObject
+            {
+                Name = "testobj",
+                Header = new ObjectHeader { ID = 1337, EntityType = EntityType.Object },
+                Position = new PSOLocation
+                {
+                    RotX = (float)3.3,
+                    RotY = (float)3.3,
+                    RotZ = (float)3.3,
+                    RotW = (float)3.3,
+                    PosX = (float)3.3,
+                    PosY = (float)3.3,
+                    PosZ = (float)3.3
+                },
+                ThingFlag = 4,
+                Things = new PSOObject.PSOObjectThing[2]
+            };
+
+            var thingData = BitConverter.ToUInt32(new byte[] { 0xff, 0xff, 0xff, 0xff }, 0);
+            testObject.Things[0] = new PSOObject.PSOObjectThing { Data = thingData };
+            var output = JsonConvert.SerializeObject(testObject);
+            Console.Out.WriteLine(output);
+        }
+    }
+
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        public void TestShiftEnum()
+        {
+            byte[] bytes = { 0x1, 0x1, 0x1 };
+            uint dataFlags = bytes[0];
+            dataFlags |= (uint)(bytes[1] << 8);
+            dataFlags |= (uint)(bytes[2] << 16);
+
+            Assert.AreEqual((PackedData.ENT1_ID | PackedData.ROT_Y | PackedData.UNK_Y),
+                (PackedData)dataFlags);
+            Console.Out.WriteLine((PackedData)dataFlags);
+        }
+
+        [Test]
+        public void TestItemGUID()
+        {
+            byte[] itemData =
+            {
+                0xDA, 0x40, 0x99, 0x60, 0x79, 0x2E, 0xFF, 0x03, 0x01, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x20, 0x00,
+                0x00, 0x03, 0xFF, 0x0A, 0x64, 0x00, 0x12, 0x34, 0x01, 0x02, 0x02, 0x02, 0x03, 0x02, 0x04, 0x02,
+                0x05, 0x02, 0x06, 0x02, 0x07, 0x02, 0x08, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+            };
+            PSO2Item item = new PSO2Item(itemData);
+            long guid = item.GetGUID();
+            long newguid = 0x0123456789ABCDEF;
+
+            Assert.AreEqual(guid, 0x03FF2E79609940DA);
+
+            item.SetGUID(newguid);
+            Assert.AreEqual(item.GetGUID(), 0x0123456789ABCDEF);
+        }
+    }
+}
