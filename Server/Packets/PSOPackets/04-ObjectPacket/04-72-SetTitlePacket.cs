d822a2acddbb3e5f3ff5cf5a176fbadcc63460e4
Longfeng Qin <sancaros@msn.cn>
2024-09-18 03:47:04 +0800

    补充数据包

---
+++ Server/Packets/PSOPackets/04-ObjectPacket/04-72-SetTitlePacket.cs
@@ -0,0 +1,31 @@
+﻿using PSO2SERVER.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSO2SERVER.Packets.PSOPackets
+{
+    public class SetTitlePacket : Packet
+    {
+
+        public SetTitlePacket()
+        {
+        }
+
+        #region implemented abstract members of Packet
+
+        public override byte[] Build()
+        {
+            var pkt = new PacketWriter();
+            return pkt.ToArray();
+        }
+
+        public override PacketHeader GetHeader()
+        {
+            return new PacketHeader(0x04, 0x72, PacketFlags.None);
+        }
+
+        #endregion
+    }
+}
